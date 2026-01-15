16f4e5c574d3291d7e37264aec8946025e2db297
Aditya Vishwakarma <ad805257@gmail.com>
2026-01-15 08:25:31 +0530

    Update Education.jsx

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -14,13 +14,13 @@ function Education() {
     <div className='mt-14 w-full mdl:w-4/5  lgl:w-3/5 h-[700px] border-l-[7px] border-l-black border-opacity-30 flex flex-col  gap-1'>
      <ResumeCard
       title="Btech in Electronics & Computer Engineering"
-      subTitle="(2025)"
-      result="8.05/10.0"
+      subTitle=""
+      result="8.35/10.0"
       des="IET,Lucknow "
       />
        <ResumeCard
       title="CITS in Electronic Mechanic"
-      subTitle="()"
+      subTitle=""
       result="91.33%"
       des="NSTI Kanpur"
       />
